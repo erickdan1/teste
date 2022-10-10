a3ceb6eb42545402328e4a807016585dd91ffdaf
Erick Daniel Alves de Lima <edal@WINDOWS.CIN.UFPE.BR>
2022-10-10 17:47:29 -0300

    seventh commit

--- jogo.py
+++ jogo.py
@@ -1,2 +1,20 @@
 print('Hello World')
 print('That is like magic')
+
+import pygame
+from pygame.locals import *
+from sys import exit
+
+largura = 640
+altura = 480
+
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('jogo')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.flip()
+    
